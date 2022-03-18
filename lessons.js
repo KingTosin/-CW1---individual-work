d8ffef3fdfd288fede83832a66bc4a60111cf9ba
Tosin <oluwatosinadio7@gmail.com>
2022-03-18 10:13:12 +0100

    second commit

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "Dubai",
+        price: 28.99,
+        image: "C:/Users/hp pavilion/Documents/vueJs classwork/New folder/images/math.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1002,
+        subject: "History",
+        location: "Rome",
+        price: 44.90,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/dog sunscreen.jfif",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1003,
+        subject: "Chemistry",
+        location: "Moscow",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1004,
+        subject: "Physics",
+        location: "Lagos",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1005,
+        subject: "French",
+        location: "Paris",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1006,
+        subject: "Spanish",
+        location: "Madrid",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1007,
+        subject: "Citizenship Education",
+        location: "Washington",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1008,
+        subject: "Technical Drawing",
+        location: "Texas",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1009,
+        subject: "Fine Arts",
+        location: "Calgary",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1010,
+        subject: "Geography",
+        location: "Berlin",
+        price: 33.10,
+        image: "C:/Users/hp pavilion/Documents/vueJs classworkweek 7/images/red-dingo-dog-collar-red.jpg",
+        availableInventory: 5,
+        rating: 3
+    }
+]
